8a37529d5b2cd3fb6a94adb21db0e8ff2f35e0b5
Gizem Fitoz <gizem.fitozuzman@coolblue.nl>
2022-06-08 16:02:00 +0200

    Initial Commit

---
+++ SwiftUI-Challenge/SwiftUI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChallengeApp.swift
+//  SwiftUI-Challenge
+//
+//  Created by Gizem Fitoz on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
